00e08c394763ef935e49286ed1027d0108cddf48
WoraponK <worapon.klabsri@gmail.com>
2024-10-11 21:05:42 +0700

    add files

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+// Lib
+import { NextPage } from 'next'
+import React from 'react'
+
+// Include in project
+import { ContentSection } from '@/containers/home'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <ContentSection />
+    </div>
+  )
+}
+
+export default Home
